cc9bad14a1e4924df05df3ecc2fee68f096de181
Melissa Lu <melissalu@fb.com>
2018-06-29 17:56:44 -0700

    Design Complete

---
+++ app/src/main/java/codepath/com/flickster/FavoritesList.java
@@ -0,0 +1,13 @@
+package codepath.com.flickster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoritesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites_list);
+    }
+}
