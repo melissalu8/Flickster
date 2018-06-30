77910727f5767611b8aab0a4e25bad4e473a1242
Melissa Lu <melissalu@fb.com>
2018-06-29 17:05:28 -0700

    Movie Trailer complete

--- app/src/main/java/codepath/com/flickster/MovieTrailerActivity.java
+++ app/src/main/java/codepath/com/flickster/MovieTrailerActivity.java
@@ -8,6 +8,6 @@ public class MovieTrailerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_trailer);
+        setContentView(R.layout.activity_movie_trailer2);
     }
 }
